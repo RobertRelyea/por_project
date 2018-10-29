a412705bde4143c99c0871cf1ac9976aa4b52a55
rer3378 <rer3378@rit.edu>
2018-10-29 14:31:58 -0400

    Created workspace, added ydlidar package and configured for x4 on /dev/ttyUSB0

---
+++ catkin_ws/build/ydlidar-master/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/imhs/principles/project/catkin_ws/src/ydlidar-master
+# Build directory: /home/imhs/principles/project/catkin_ws/build/ydlidar-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
